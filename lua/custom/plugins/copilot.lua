b59ee83e6a7db2b504240592abac90cf8a10d4e0
Christie Koehler <chkoehle@cisco.com>
2025-09-08 14:59:57 -0700

    Add mapping for CoPilot. (required for work)

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,7 +3,8 @@ return {
   dependencies = { 'catppuccin/nvim' },
   event = 'VimEnter',
   init = function()
-    vim.g.copilot_no_tab_map = false
+    vim.g.copilot_no_tab_map = true
+    vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
     vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
   end,
   keys = {},
--- lua/custom/plugins/copilotchat.lua
+++ lua/custom/plugins/copilotchat.lua
@@ -9,6 +9,17 @@ return {
     opts = {
       -- See Configuration section for options
     },
+    keys = {
+      { '<leader>ae', ':CopilotChatExplain<CR>', mode = 'v', desc = 'Explain Code' },
+      { '<leader>ar', ':CopilotChatReview<CR>', mode = 'v', desc = 'Review Code' },
+      { '<leader>af', ':CopilotChatFix<CR>', mode = 'v', desc = 'Fix Code Issues' },
+      { '<leader>ad', ':CopilotChatDocs<CR>', mode = 'v', desc = 'Generate Docs' },
+      { '<leader>ao', ':CopilotChatOptimize<CR>', mode = 'v', desc = 'Optimize Code' },
+      { '<leader>at', ':CopilotChatTests<CR>', mode = 'v', desc = 'Generate Tests' },
+      { '<leader>ac', ':CopilotChatCommit<CR>', mode = 'v', desc = 'Generate Commit Message' },
+      { '<leader>am', ':CopilotChatModels<CR>', mode = 'n', desc = 'Change Model' },
+      { '<leader>ap', ':CopilotChatPrompts<CR>', mode = 'v', desc = 'Change Prompt' },
+    },
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
